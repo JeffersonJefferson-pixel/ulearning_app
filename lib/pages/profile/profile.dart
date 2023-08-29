3f3a6ff7f96afa027e0a65b90e62cb578edf9532
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-08-29 21:30:04 +0700

    add profile page

---
+++ lib/pages/profile/profile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/common/values/colors.dart';
+import 'package:ulearning_app/pages/profile/widgets/profile_widgets.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryBackground,
+      appBar: buildAppBar(),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              profileIcon(),
+              Padding(
+                padding: EdgeInsets.only(left: 25.w),
+                child: buildListView(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
